4a26cdbc4a3ff2e48fe01140d7368a9c54cb3eab
Ian <ianmcm.89@gmail.com>
2022-11-10 04:59:38 -0500

    completed assignment

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,9 +1,17 @@
 function reverseString(str) {
-  // type your code here
+  let result = [];
+  [...str].forEach((e, i) => result.push(str[(str.length-1)-i]));
+  // console.log(result);
+  return result.join('');
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 'gnirts'");
+  console.log("=>", reverseString("string"));
+
+  console.log("");
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -16,4 +24,17 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+/**************************************************
+ * initialize empty array called result
+ * 
+ * convert string to array and iterate over each item:
+ *    new index of item equals length of string decremented by 1 minus the item index
+ *    push item into result
+ * 
+ * return result array
+ * ************************************************/
+
 // And a written explanation of your solution
+/**************************************************
+
+ * ************************************************/
